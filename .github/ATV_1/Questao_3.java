871b8b6ebe967bff53643ad72bdb987341cab0a4
FThierryDev <97753490+FThierryDev@users.noreply.github.com>
2022-09-26 20:19:09 -0300

    Delete .github/ATV_1 directory

--- .github/ATV_1/Questao_3.java
+++
@@ -1,6 +0,0 @@
-class Questao_3 {
-    public static void main(String[] args) {
-        System.out.print("Terminei a primeira aula com um programa Java!");
-
-    }
-}
--- .github/ATV_1/Questao_4.java
+++
@@ -1,9 +0,0 @@
-class Questao_4 {
-    public static void main(String[] args) {
-        System.out.println("Thierry");
-        System.out.println("Flamengo\n");
-
-        System.out.println("Richard");
-        System.out.println("Inazuma Eleven");
-    }
-}
